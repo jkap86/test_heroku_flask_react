13c3a584ae0d8f3cf11d7eaf407cff07b665c0d7
jkap86 <jkap86@gmail.com>
2022-10-07 12:26:12 -0400

    -

--- server/app.py
+++ server/app.py
@@ -2,8 +2,7 @@ from flask import Flask
 import os
 
 dir = os.path.dirname(__file__)
-app = Flask(__name__, static_folder='../client/build/static',
-            static_url_path='')
+app = Flask(__name__, template_folder='../client/build')
 
 
 @app.route('/test')
